9d5427570e60d03a5c3c046ab15d144f14e64a36
felcg <xfelipecg@gmail.com>
2020-04-29 21:11:05 -0300

    3.22: Lint configuration

--- part3/notes/backend/mongo.js
+++ part3/notes/backend/mongo.js
@@ -1,38 +1,38 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log("give password as argument");
-  process.exit(1);
+  console.log('give password as argument')
+  process.exit(1)
 }
 
-const password = process.argv[2];
+const password = process.argv[2]
 
-const url = `mongodb+srv://admin:${password}@cluster0-rcxcj.mongodb.net/note-app?retryWrites=true&w=majority`;
+const url = `mongodb+srv://admin:${password}@cluster0-rcxcj.mongodb.net/note-app?retryWrites=true&w=majority`
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const noteSchema = new mongoose.Schema({
   content: String,
   date: Date,
   important: Boolean,
-});
+})
 
-const Note = mongoose.model("Note", noteSchema);
+const Note = mongoose.model('Note', noteSchema)
 
 const note = new Note({
-  content: "React is starting to feel just right",
+  content: 'React is starting to feel just right',
   date: new Date(),
   important: true,
-});
+})
 
-// note.save().then((response) => {
-//   console.log("note saved!");
-//   mongoose.connection.close();
-// });
+note.save().then(() => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
 
 Note.find({}).then((result) => {
-  result.forEach((note) => {
-    console.log(note);
-  });
-  mongoose.connection.close();
-});
+  result.forEach(() => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
