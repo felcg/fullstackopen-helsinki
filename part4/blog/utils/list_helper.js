b78d12d6d8d2a44cbea3439c18ab62cf68ed9e64
felcg <xfelipecg@gmail.com>
2020-05-01 00:37:54 -0300

    4.5*: helper functions and unit tests, step3

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,11 +1,30 @@
 // eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => 1
 
-const totalLikes = (blogs) => {
-    const sumLikes = (sum, item) => sum + item.likes
-    return blogs.reduce(sumLikes, 0)
+const totalLikes = (blogs) => (blogs.length === 0
+    ? 0
+    : blogs.map((blog) => blog.likes).reduce((sum, item) => sum + item))
+
+const favoriteBlog = (blogs) => {
+    let likes = 0
+    // eslint-disable-next-line no-shadow
+    let favoriteBlog = {}
+
+    blogs.forEach((blog) => {
+        if (blog.likes > likes) {
+            likes = blog.likes
+            favoriteBlog = blog
+        }
+    })
+
+    return {
+        title: favoriteBlog.title,
+        author: favoriteBlog.author,
+        likes: favoriteBlog.likes,
+    }
 }
 
+
 module.exports = {
-    dummy, totalLikes,
+    dummy, totalLikes, favoriteBlog,
 }
