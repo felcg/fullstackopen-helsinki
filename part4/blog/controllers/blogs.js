ccc49ef931eed8ee970a3b5932fabe77d0dd0a73
felcg <xfelipecg@gmail.com>
2020-05-05 00:43:26 -0300

    4.20*: bloglist expansion, step8

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -3,13 +3,6 @@ const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
-const getTokenFrom = (request) => {
-    const authorization = request.get('authorization')
-    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-        return authorization.substring(7)
-    }
-    return null
-}
 
 blogsRouter.get('/', async (request, response, next) => {
     try {
@@ -21,10 +14,8 @@ blogsRouter.get('/', async (request, response, next) => {
 })
 
 blogsRouter.post('/', async (request, response, next) => {
-    const { body } = request
-
+    const { body, token } = request
     try {
-        const token = getTokenFrom(request)
         const decodedToken = jwt.verify(token, process.env.SECRET)
         if (!token || !decodedToken.id) {
             return response.status(401).json({ error: 'token missing or invalid' })
