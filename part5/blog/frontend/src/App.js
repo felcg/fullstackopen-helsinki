b7cdf0304ab5346af35124fe5345c52ee3858600
felcg <xfelipecg@gmail.com>
2020-05-05 22:41:19 -0300

    5.2: bloglist frontend, step2

--- part5/blog/frontend/src/App.js
+++ part5/blog/frontend/src/App.js
@@ -14,6 +14,14 @@ const App = () => {
     blogService.getAll().then((blogs) => setBlogs(blogs))
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogeappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
@@ -22,7 +30,7 @@ const App = () => {
         password,
       })
 
-      // window.localStorage.setItem('loggedNoteappUser', JSON.stringify(user))
+      window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
 
       // blogService.setToken(user.token)
       setUser(user)
@@ -36,6 +44,11 @@ const App = () => {
       //   setErrorMessage(null);
       // }, 5000);
     }
+  }
+
+  const logout = () => {
+    window.localStorage.clear()
+    setUser(null)
   }
 
   return (
@@ -53,6 +66,7 @@ const App = () => {
         <div>
           <h2>blogs</h2>
           <p>{user.name} logged in</p>
+          <button type="button" onClick={logout}>logout</button>
           {blogs.map((blog) => (
             <Blog key={blog.id} blog={blog} />
           ))}
