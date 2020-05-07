9a62ddefc8f2d9a864e67461dbcb1e2aa45c1c87
felcg <xfelipecg@gmail.com>
2020-05-07 20:38:23 -0300

    5.8*: Blog list frontend, step8

--- part5/blog/frontend/src/components/Blog.js
+++ part5/blog/frontend/src/components/Blog.js
@@ -1,23 +1,36 @@
 /* eslint-disable react/no-unescaped-entities */
 import React, { useState } from 'react'
+import blogService from '../services/blogs'
 import '../App.css'
 
 const Blog = ({ blog }) => {
   const [visible, setVisible] = useState(false)
+  const [likes, setLikes] = useState(blog.likes)
   const hideWhenVisible = { display: visible ? 'none' : '' }
   const showWhenVisible = { display: visible ? '' : 'none' }
+
   const blogStyle = {
     paddingTop: 10,
     paddingLeft: 2,
     border: 'solid',
     borderWidth: 1,
     marginBottom: 2,
+    width: 400,
   }
 
   const flex = { display: 'flex' }
 
   const toggleVisibility = () => {
     setVisible(!visible)
+  }
+
+  // esse método recebe o blog atual e aumenta em 1 o seus likes
+  // em seguida usa o blogService para fazer o PUT request e entao
+  // atualiza o state dos likes
+  const addLike = async (blog) => {
+    blog.likes += 1
+    const response = await blogService.updateLikes(blog)
+    setLikes(response.likes)
   }
 
   return (
@@ -37,7 +50,12 @@ const Blog = ({ blog }) => {
             <button onClick={toggleVisibility}>hide</button>
           </div>
           <p>{blog.url}</p>
-          <div style={flex}><p>{blog.likes}</p> <button>like</button></div>
+          <div style={flex}>
+            <p>{likes}</p>
+            {/* se colocasse addLike(blog) direto ao invés de
+             () => addLike() o onlick dispararia para todos os blogs */}
+            <button onClick={() => addLike(blog)}>like</button>
+          </div>
           <p>{blog.user.username}</p>
 
         </div>
--- part5/blog/frontend/src/services/blogs.js
+++ part5/blog/frontend/src/services/blogs.js
@@ -10,9 +10,9 @@ const setToken = (newToken) => {
   token = `bearer ${newToken}`
 }
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then((response) => response.data)
+const getAll = async () => {
+  const response = await axios.get(baseUrl)
+  return response.data
 }
 
 const create = async (newObject) => {
@@ -24,9 +24,18 @@ const create = async (newObject) => {
   }
 
   const response = await axios.post(baseUrl, newObject, config)
-
-
   return response.data
 }
 
-export default { getAll, setToken, create }
+const updateLikes = async (updatedBlog) => {
+  // updateLikes recebe um blog como parametro e então
+  // envia um PUT request para a api usando o id recebido
+  // nos parametros. e passa o Objeto inteiro com os Likes
+  // atualizados
+  const response = await axios.put(`${baseUrl}/${updatedBlog.id}`, updatedBlog)
+  return response.data
+}
+
+export default {
+  getAll, setToken, create, updateLikes,
+}
