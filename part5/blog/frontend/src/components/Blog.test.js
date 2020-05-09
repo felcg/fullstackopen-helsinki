a0b2acf83f87e335fa526eadf71a6bafd96e468c
felcg <xfelipecg@gmail.com>
2020-05-08 21:18:58 -0300

    5.14: Blog list tests, step2

--- part5/blog/frontend/src/components/Blog.test.js
+++ part5/blog/frontend/src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 describe('Blog', () => {
@@ -31,5 +31,13 @@ describe('Blog', () => {
     const partialBlog = component.container.querySelector('.blogPartial')
     expect(partialBlog).not.toHaveTextContent('like')
     expect(partialBlog).not.toHaveTextContent(blog.url)
+  })
+
+  test('blog renders url and likes after revealing it', () => {
+    const button = component.container.querySelector('button')
+    fireEvent.click(button)
+
+    expect(component.container).toHaveTextContent('like')
+    expect(component.container).toHaveTextContent(blog.url)
   })
 })
