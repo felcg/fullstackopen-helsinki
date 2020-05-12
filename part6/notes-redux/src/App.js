9ec8fcc2cfd076632e9f565362f8edd302111105
felcg <xfelipecg@gmail.com>
2020-05-12 05:04:38 -0300

    Part6 b Many reducers

--- part6/notes-redux/src/App.js
+++ part6/notes-redux/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import NewNote from './components/NewNote'
 import Notes from './components/Notes'
+import VisibilityFilter from './components/VisibilityFilter'
 
 const App = () => (
   <div>
     <NewNote />
+    <VisibilityFilter />
     <Notes />
   </div>
 )
