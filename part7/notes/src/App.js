7623e4546305ef933434f0ddf6cf2ce6135cc0a4
felcg <xfelipecg@gmail.com>
2020-05-18 11:13:20 -0300

    Part7 More about styles

--- part7/notes/src/App.js
+++ part7/notes/src/App.js
@@ -1,5 +1,19 @@
 import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+import {
+  Container,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Paper,
+  TextField,
+  AppBar,
+  IconButton,
+  Toolbar
+} from '@material-ui/core'
+import styled from 'styled-components'
+import { Alert } from '@material-ui/lab'
 
 import {
   BrowserRouter as Router,
@@ -10,6 +24,35 @@ import {
   useRouteMatch,
   useHistory,
 } from "react-router-dom"
+
+const Button = styled.button`
+  background: Bisque;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid Chocolate;
+  border-radius: 3px;
+`
+
+const Input = styled.input`
+  margin: 0.25em;
+`
+
+const Page = styled.div`
+  padding: 1em;
+  background: papayawhip;
+`
+
+const Navigation = styled.div`
+  background: BurlyWood;
+  padding: 1em;
+`
+
+const Footer = styled.div`
+  background: Chocolate;
+  padding: 1em;
+  margin-top: 1em;
+`
 
 const Home = () => (
   <div> 
@@ -31,13 +74,23 @@ const Note = ({ note }) => {
 const Notes = ({notes}) => (
   <div>
     <h2>Notes</h2>
-    <ul>
-      {notes.map(note =>
-        <li key={note.id}>
-          <Link to={`/notes/${note.id}`}>{note.content}</Link>
-        </li>
-      )}
-    </ul>
+
+    <TableContainer component={Paper}>
+      <Table>
+        <TableBody>
+          {notes.map(note => (
+            <TableRow key={note.id}>
+              <TableCell>
+                <Link to={`/notes/${note.id}`}>{note.content}</Link>
+              </TableCell>
+              <TableCell>
+                {note.name}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   </div>
 )
 
@@ -66,12 +119,14 @@ const Login = (props) => {
       <h2>login</h2>
       <form onSubmit={onSubmit}>
         <div>
-          username: <input />
+          username:
+          <Input />
         </div>
         <div>
-          password: <input type='password' />
+          password:
+          <Input type='password' />
         </div>
-        <button type="submit">login</button>
+        <Button type="submit" primary=''>login</Button>
       </form>
     </div>
   )
@@ -100,9 +155,14 @@ const App = () => {
   ])
 
   const [user, setUser] = useState(null) 
+  const [message, setMessage] = useState(null)
 
   const login = (user) => {
     setUser(user)
+    setMessage(`welcome ${user}`)
+    setTimeout(() => {
+      setMessage(null)
+    }, 10000)
   }
 
   const padding = {
@@ -115,8 +175,8 @@ const App = () => {
     : null
 
   return (
-    <div>
-      <div>
+    <Page>
+      <Navigation>
         <Link style={padding} to="/">home</Link>
         <Link style={padding} to="/notes">notes</Link>
         <Link style={padding} to="/users">users</Link>
@@ -124,7 +184,7 @@ const App = () => {
           ? <em>{user} logged in</em>
           : <Link style={padding} to="/login">login</Link>
         }
-      </div>
+      </Navigation>
 
       <Switch>
         <Route path="/notes/:id">
@@ -143,19 +203,12 @@ const App = () => {
           <Home />
         </Route>
       </Switch>
-      <div>
-        <br />
+      
+      <Footer>
         <em>Note app, Department of Computer Science 2020</em>
-      </div>
-    </div>
+      </Footer>
+    </Page>
   )
 }
-
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
-)
 
 export default App
