43cf769df59a5b11f48fba951647f99528fbd38e
felcg <xfelipecg@gmail.com>
2020-05-23 15:38:34 -0300

    8.5: Books by genre

--- part8/bookapp/index.js
+++ part8/bookapp/index.js
@@ -103,7 +103,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String!): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -112,7 +112,21 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: (root, args) => books.filter((book) => book.author === args.author),
+    allBooks: (root, args) => {
+      if (!args) {
+        return books;
+      }
+      if (args.author && args.genre) {
+        return books
+          .filter((book) => book.author === args.author)
+          .filter((book) => book.genres.includes(args.genre));
+      } if (args.author) {
+        return books.filter((book) => book.author === args.author);
+      } if (args.genre) {
+        return books.filter((book) => book.genres.includes(args.genre));
+      }
+      return books;
+    },
     allAuthors: () => authors.map((author) => (
       { ...author, bookCount: books.filter((book) => book.author === author.name).length }
     )),
